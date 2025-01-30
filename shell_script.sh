203ea31f5009bfac5b9dd09bf06d733c90ef0e03
Abhishek <abhishekkanawade2000@gmai.com>
2025-01-30 13:15:52 +0530

    my_first_commit

---
+++ shell_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#########################################################
+# Author: Abhishek
+# Date: 29-01-2025
+# Version: V1
+# 
+# This script will report the AWS Resource usage.
+#########################################################
