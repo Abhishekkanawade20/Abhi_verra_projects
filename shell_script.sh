702840cc851238c1c70ff44e13f5b6e7d01c079b
Abhishek <abhishekkanawade2000@gmai.com>
2025-02-03 16:05:46 +0530

    Remove one echo line

--- shell_script.sh
+++ shell_script.sh
@@ -5,10 +5,10 @@
 # Date: 29-01-2025
 # Version: V1
 # 
-# This script will report the AWS Resource usage.
+# This script will report the AWS Resource usage.we are going to list of instance.
 #########################################################
 
-echo "list of aws instance"
+
 echo "below are the list of instnace"
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
 
