b7feee7244f1d1ee8103d64dcd7f0f06df260339
Abhishek <abhishekkanawade2000@gmai.com>
2025-04-24 13:01:07 +0530

    creating another ec2 instance

---
+++ Terraform_labs/ec2/ec2.tf
@@ -0,0 +1,13 @@
+# This is test file 
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_instance" "dev_instance" {
+  ami = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+}
+
+# adding new line 
