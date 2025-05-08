7d4b092506e21204a1d83ccca49e0adf69ed5698
Abhishek <abhishekkanawade2000@gmai.com>
2025-05-08 14:28:16 +0530

    add some changes in the backend file

--- Terraform_labs/day-4/backend.tf
+++ Terraform_labs/day-4/backend.tf
@@ -3,6 +3,6 @@ terraform {
   backend "s3" {
     region = "ap-south-1"
     bucket = "abhi-bucket-08052025"   
-    key = "abhi-bucket-08052025/abhishek"
+    key = "abhishek/terraform.tfstate"
   }
 }
--- Terraform_labs/day-4/terraform.tfvars
+++ Terraform_labs/day-4/terraform.tfvars
@@ -1,6 +1,6 @@
 
 ami = "ami-0e35ddab05955cf57"
 region = "ap-south-1"
-bucket = "abhi-bucket-08052025"
+# bucket = "abhi-bucket-08052025"
 instance_type = "t2.micro"
 
