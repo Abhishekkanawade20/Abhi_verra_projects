f2fe53afc9f9fee4d86df2d6daa8212edd9289b1
Abhishek <abhishekkanawade2000@gmai.com>
2025-05-12 19:21:35 +0530

    modified types of variable in variables.tf file

--- Terraform_labs/day-5/variables.tf
+++ Terraform_labs/day-5/variables.tf
@@ -1,15 +1,15 @@
 
 variable "cidr" {
   description = " value of cidr"
-  type = "string"
+  type = number
 }
 
 variable "cidr_block" {
     description = "value of cidr_block"
-    type = "string"
+    type = number
 }
 
 variable "map_public_ip_on_launch" {
  description = "True or false"  
- type = "bool"
+ type = bool
 }
