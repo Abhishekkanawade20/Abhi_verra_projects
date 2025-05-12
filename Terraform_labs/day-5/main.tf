a46e5057e1574fdd1b426111401bf9963108e22a
Abhishek <abhishekkanawade2000@gmai.com>
2025-05-12 17:53:44 +0530

    write a vpc creation terraform module

---
+++ Terraform_labs/day-5/main.tf
@@ -0,0 +1,30 @@
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.cidr
+}
+
+resource "aws_subnet" "subnet1" {
+    vpc_id = aws_vpc.myvpc
+    cidr_block = var.cidr_block
+    availability_zone = "ap-south-1"
+    map_public_ip_on_launch = var.map_public_ip_on_launch
+}
+
+resource "aws_internet_gateway" "IGW" {
+    vpc_id = aws_vpc.myvpc
+}
+
+resource "aws_route_table" "rt" {
+    vpc_id = aws_vpc.myvpc
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.IGW
+    }
+  
+}
+
---
+++ Terraform_labs/day-5/variables.tf
@@ -0,0 +1,12 @@
+
+variable "cidr" {
+  description = " value of cidr"
+}
+
+variable "cidr_block" {
+    description = "value of cidr_block"
+}
+
+variable "map_public_ip_on_launch" {
+ description = "True or false"  
+}
