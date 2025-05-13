38d7487207409496a48303fa44b81e7521f98aca
Abhishek <abhishekkanawade2000@gmai.com>
2025-05-13 20:23:33 +0530

    added provisioner and app.py

--- Terraform_labs/day-5/main.tf
+++ Terraform_labs/day-5/main.tf
@@ -102,7 +102,7 @@ resource "aws_instance" "name" {
         "sudo apt update -y",  # Update package lists (for ubuntu)
         "sudo apt-get install -y python3-pip",  # Example package installation
         "cd /home/ubuntu",
-        "sudo pip3 install flask",
+        "sudo apt install flask",
         "sudo python3 app.py &"
          ]
     }
