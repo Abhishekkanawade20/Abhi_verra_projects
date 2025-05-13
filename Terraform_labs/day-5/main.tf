ff9316636566a301e20381a697d24ff3f9336d87
Abhishek <abhishekkanawade2000@gmai.com>
2025-05-13 18:52:05 +0530

    m

--- Terraform_labs/day-5/main.tf
+++ Terraform_labs/day-5/main.tf
@@ -82,6 +82,6 @@ resource "aws_instance" "name" {
     instance_type = var.instance_type
     key_name = aws_key_pair.example.key_name
     subnet_id = aws_subnet.subnet1.id
-    vpc_security_group_ids = [aws_security_group.ssh.id]
-}
+    vpc_security_group_ids = [aws_security_group.webSg.id]
+    }
 
